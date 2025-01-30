2d7a7592fbe8f28fdb48eae10487afdc79cb8323
Jason Lee <huacnlee@gmail.com>
2025-01-30 10:52:41 +0800

    .

--- crates/ui/src/input/element.rs
+++ crates/ui/src/input/element.rs
@@ -506,7 +506,11 @@ impl Element for TextElement {
         let mut offset_y = px(0.);
         if self.input.read(cx).masked {
             // Move down offset for vertical centering the *****
-            offset_y = px(2.5);
+            if cfg!(target_os = "macos") {
+                offset_y = px(3.);
+            } else {
+                offset_y = px(2.5);
+            }
         }
         for line in prepaint.lines.iter() {
             let p = point(origin.x, origin.y + offset_y);
