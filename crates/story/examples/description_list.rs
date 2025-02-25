2a072d86e794d4e5d0ea810c06201fb7c7e9101e
Jason Lee <huacnlee@gmail.com>
2025-02-25 13:58:17 +0800

    Add description_list

---
+++ crates/story/examples/description_list.rs
@@ -0,0 +1,156 @@
+use gpui::*;
+use gpui_component::{
+    button::Button,
+    checkbox::Checkbox,
+    description_list::{DescriptionItem, DescriptionList},
+    h_flex,
+    popup_menu::PopupMenuExt as _,
+    v_flex, AxisExt, Sizable as _, Size,
+};
+use serde::Deserialize;
+use story::Assets;
+
+#[derive(Clone, PartialEq, Eq, Deserialize)]
+struct ChangeSize(Size);
+
+impl_internal_actions!(example, [ChangeSize]);
+
+pub struct Example {
+    layout: Axis,
+    bordered: bool,
+    size: Size,
+    items: Vec<(&'static str, &'static str, usize)>,
+}
+
+impl Example {
+    pub fn new(_: &mut Window, _: &mut Context<Self>) -> Self {
+        let items = vec![
+            ("Name", "GPUI Component", 1),
+            (
+                "Description",
+                "UI components for building fantastic desktop application by using GPUI.",
+                3,
+            ),
+            ("Version", "0.1.0", 1),
+            ("License", "Apache-2.0", 1),
+            ("Author", "Longbridge", 1),
+            (
+                "Repository",
+                "https://github.com/longbridge/gpui-component",
+                1,
+            ),
+        ];
+
+        Self {
+            items,
+            bordered: true,
+            size: Size::default(),
+            layout: Axis::Horizontal,
+        }
+    }
+
+    fn view(window: &mut Window, cx: &mut App) -> Entity<Self> {
+        cx.new(|cx| Self::new(window, cx))
+    }
+
+    fn set_layout(&mut self, layout: Axis, cx: &mut Context<Self>) {
+        self.layout = layout;
+        cx.notify();
+    }
+
+    fn set_bordered(&mut self, bordered: bool, cx: &mut Context<Self>) {
+        self.bordered = bordered;
+        cx.notify();
+    }
+
+    fn on_change_size(&mut self, a: &ChangeSize, _: &mut Window, cx: &mut Context<Self>) {
+        self.size = a.0;
+        cx.notify();
+    }
+}
+
+impl Render for Example {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        v_flex()
+            .id("exmaple")
+            .on_action(cx.listener(Self::on_change_size))
+            .p_4()
+            .size_full()
+            .gap_2()
+            .child(
+                h_flex()
+                    .gap_3()
+                    .child(
+                        Checkbox::new("layout")
+                            .checked(self.layout.is_vertical())
+                            .label("Vertical Layout")
+                            .on_click(cx.listener(|this, checked: &bool, _, cx| {
+                                let new_layout = if *checked {
+                                    Axis::Vertical
+                                } else {
+                                    Axis::Horizontal
+                                };
+                                this.set_layout(new_layout, cx);
+                            })),
+                    )
+                    .child(
+                        Checkbox::new("bordered")
+                            .checked(self.bordered)
+                            .label("Bordered")
+                            .on_click(cx.listener(|this, checked: &bool, _, cx| {
+                                this.set_bordered(*checked, cx);
+                            })),
+                    )
+                    .child(
+                        Button::new("size")
+                            .small()
+                            .label(format!("size: {:?}", self.size))
+                            .popup_menu({
+                                let size = self.size;
+                                move |menu, _, _| {
+                                    menu.menu_with_check(
+                                        "Large",
+                                        size == Size::Large,
+                                        Box::new(ChangeSize(Size::Large)),
+                                    )
+                                    .menu_with_check(
+                                        "Medium",
+                                        size == Size::Medium,
+                                        Box::new(ChangeSize(Size::Medium)),
+                                    )
+                                    .menu_with_check(
+                                        "Small",
+                                        size == Size::Small,
+                                        Box::new(ChangeSize(Size::Small)),
+                                    )
+                                }
+                            }),
+                    ),
+            )
+            .child(
+                DescriptionList::new()
+                    .columns(3)
+                    .layout(self.layout)
+                    .bordered(self.bordered)
+                    .with_size(self.size)
+                    .children(self.items.clone().into_iter().map(|(label, value, span)| {
+                        DescriptionItem::Item {
+                            label: label.into(),
+                            value: div().child(value).into_any_element(),
+                            span,
+                        }
+                    })),
+            )
+    }
+}
+
+fn main() {
+    let app = Application::new().with_assets(Assets);
+
+    app.run(move |cx| {
+        story::init(cx);
+        cx.activate(true);
+
+        story::create_new_window("Description List Example", Example::view, cx);
+    });
+}
