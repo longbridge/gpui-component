10acd6ad2d1781af51c9038b64574be002397fa8
Jason Lee <huacnlee@gmail.com>
2025-03-05 17:23:33 +0800

    tab: Add `tab_bar_segemented` theme variable.

--- crates/ui/src/tab/tab_bar.rs
+++ crates/ui/src/tab/tab_bar.rs
@@ -152,7 +152,7 @@ impl RenderOnce for TabBar {
                     Size::XSmall => px(3.),
                     Size::Small => px(3.),
                     Size::Large => px(6.),
-                    _ => px(4.),
+                    _ => px(5.),
                 };
                 let padding = Edges {
                     left: padding_x,
@@ -160,7 +160,7 @@ impl RenderOnce for TabBar {
                     ..Default::default()
                 };
 
-                (cx.theme().tab_bar, padding, px(2.))
+                (cx.theme().tab_bar_segmented, padding, px(2.))
             }
             TabVariant::Underline => {
                 let padding = Edges::all(px(0.));
