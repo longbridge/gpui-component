e16d4ef1dddc4ee4bb2aa88b75676d3828fbaa67
高庆丰 <gao.qingfeng@gmail.com>
2025-07-21 14:27:04 +0800

    Update window.rs

--- crates/todo-app-gpui/examples/window.rs
+++ crates/todo-app-gpui/examples/window.rs
@@ -3,6 +3,7 @@ use gpui::{
     PromptButton, PromptLevel, SharedString, Timer, Window, WindowBounds, WindowKind,
     WindowOptions,
 };
+use trayicon::*;
 
 struct SubWindow {
     custom_titlebar: bool,
@@ -212,6 +213,121 @@ impl Render for WindowDemo {
 actions!(window, [Quit]);
 
 fn main() {
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    enum Events {
+        RightClickTrayIcon,
+        LeftClickTrayIcon,
+        DoubleClickTrayIcon,
+        Exit,
+        Item1,
+        Item2,
+        Item3,
+        Item4,
+        CheckItem1,
+        SubItem1,
+        SubItem2,
+        SubItem3,
+    }
+
+    let (s, r) = std::sync::mpsc::channel();
+    let icon = include_bytes!("../../../assets/logo2.ico");
+    let icon2 = include_bytes!("../../../assets/logo2.ico");
+
+    let second_icon = Icon::from_buffer(icon2, None, None).unwrap();
+    let first_icon = Icon::from_buffer(icon, None, None).unwrap();
+
+    // Needlessly complicated tray icon with all the whistles and bells
+    let mut tray_icon = TrayIconBuilder::new()
+        .sender(move |e| {
+            let _ = s.send(*e);
+        })
+        .icon_from_buffer(icon)
+        .tooltip("Cool Tray 👀 Icon")
+        .on_right_click(Events::RightClickTrayIcon)
+        .on_click(Events::LeftClickTrayIcon)
+        // .on_double_click(Events::DoubleClickTrayIcon)
+        .menu(
+            MenuBuilder::new()
+                .item("Item 3 Replace Menu 👍", Events::Item3)
+                .item("Item 2 Change Icon Green", Events::Item2)
+                .item("Item 1 Change Icon Red", Events::Item1)
+                .separator()
+                .checkable("This is checkable", true, Events::CheckItem1)
+                .submenu(
+                    "Sub Menu",
+                    MenuBuilder::new()
+                        .item("Sub item 1", Events::SubItem1)
+                        .item("Sub Item 2", Events::SubItem2)
+                        .item("Sub Item 3", Events::SubItem3),
+                )
+                .with(MenuItem::Item {
+                    name: "Item Disabled".into(),
+                    disabled: true, // Disabled entry example
+                    id: Events::Item4,
+                    icon: None,
+                })
+                .separator()
+                .item("E&xit", Events::Exit),
+        )
+        .build()
+        .unwrap();
+
+    std::thread::spawn(move || {
+        r.iter().for_each(|m| match m {
+            Events::RightClickTrayIcon => {
+                tray_icon.show_menu().unwrap();
+            }
+            Events::DoubleClickTrayIcon => {
+                println!("Double click");
+            }
+            Events::LeftClickTrayIcon => {
+                println!("Left Click click");
+                // tray_icon.show_menu().unwrap();
+            }
+            Events::Exit => {
+                println!("Please exit");
+                std::process::exit(0);
+            }
+            Events::Item1 => {
+                tray_icon.set_icon(&second_icon).unwrap();
+            }
+            Events::Item2 => {
+                tray_icon.set_icon(&first_icon).unwrap();
+            }
+            Events::Item3 => {
+                tray_icon
+                    .set_menu(
+                        &MenuBuilder::new()
+                            .item("New menu item", Events::Item1)
+                            .item("Exit", Events::Exit),
+                    )
+                    .unwrap();
+            }
+            e => {
+                println!("{:?}", e);
+            }
+        })
+    });
+
+    // Your applications message loop. Because all applications require an
+    // application loop, you are best served using an `winit` crate.
+    std::thread::spawn(move || {
+        use windows::Win32::UI::WindowsAndMessaging::*;
+        let mut msg = MSG::default();
+        loop {
+            unsafe {
+                let ret = GetMessageW(&mut msg, None, 0, 0);
+
+                if ret.as_bool() {
+                    let _ = TranslateMessage(&msg);
+                    let _ = DispatchMessageA(&msg);
+                } else {
+                    break;
+                }
+            }
+        }
+    });
+
     Application::new().run(|cx: &mut App| {
         let bounds = Bounds::centered(None, size(px(800.0), px(600.0)), cx);
 
