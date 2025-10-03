6322bcbfdb5b2bb0e208045ba093c5023036218a
Jason Lee <huacnlee@gmail.com>
2025-10-03 10:13:34 +0800

    .

--- crates/ui/src/button/button.rs
+++ crates/ui/src/button/button.rs
@@ -533,7 +533,6 @@ impl RenderOnce for Button {
             .child({
                 h_flex()
                     .id("label")
-                    .overflow_hidden()
                     .items_center()
                     .justify_center()
                     .button_text_size(self.size)
