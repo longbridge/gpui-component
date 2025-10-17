e4fd03d1ba143808f07d193b2db2ed50f015b8d2
Jason Lee <huacnlee@gmail.com>
2025-10-17 10:09:42 +0800

    .lint

--- crates/ui/src/input/lsp/hover.rs
+++ crates/ui/src/input/lsp/hover.rs
@@ -48,7 +48,7 @@ impl InputState {
         let task = provider.hover(&self.text, offset, window, cx);
         let mut symbol_range = self.text.word_range(offset).unwrap_or(offset..offset);
         let editor = cx.entity();
-        let should_delay = !self.hover_popover.is_some();
+        let should_delay = self.hover_popover.is_none();
         self.lsp._hover_task = cx.spawn_in(window, async move |_, cx| {
             if should_delay {
                 cx.background_executor()
