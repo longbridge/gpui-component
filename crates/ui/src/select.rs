f721fe6a68a05fa17a498a6f783fad447049ab58
Floyd Wang <gassnake999@gmail.com>
2026-04-13 16:59:44 +0800

    Add early return

--- crates/ui/src/select.rs
+++ crates/ui/src/select.rs
@@ -720,14 +720,12 @@ where
     fn escape(&mut self, _: &Cancel, window: &mut Window, cx: &mut Context<Self>) {
         if !self.open {
             cx.propagate();
-        } else {
-            cx.stop_propagation();
+            return;
         }
 
+        cx.stop_propagation();
         self.set_open(false, cx);
-
         self.focus_handle.focus(window, cx);
-
         cx.notify();
     }
 
