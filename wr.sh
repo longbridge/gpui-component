c22213e9493a6c1f4af25eefe14c30e37a3721cb
elcoosp <elcoosp@gmail.com>
2026-04-21 18:26:08 +0200

    fix: rm wr.sh

--- wr.sh
+++
@@ -1,4 +0,0 @@
-sed -i '' \
-  -e 's/\.label(cx\.theme()\.notification\.placement\.to_string())/.label(format!("{:?}", cx.theme().notification.placement))/' \
-  -e 's/PopupMenuItem::new(placement\.to_string())/PopupMenuItem::new(format!("{:?}", placement))/' \
-  crates/story/src/stories/notification_story.rs
