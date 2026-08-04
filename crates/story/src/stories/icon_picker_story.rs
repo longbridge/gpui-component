a44dd40ade8c707038ab814c0646459db1282694
cacia <caciaaicac@gmail.com>
2026-08-04 20:36:29 +0200

    Expanded the gallery story

--- crates/story/src/stories/icon_picker_story.rs
+++ crates/story/src/stories/icon_picker_story.rs
@@ -1,5 +1,5 @@
 use gpui::{
-    App, AppContext, Context, Entity, Focusable, IntoElement, ParentElement as _, Render,
+    App, AppContext, Context, Entity, Focusable, Hsla, IntoElement, ParentElement as _, Render,
     RenderOnce, SharedString, Styled as _, Subscription, Window, div, prelude::FluentBuilder,
 };
 use gpui_component::{
@@ -20,8 +20,10 @@ impl RenderOnce for PickableIcon {
 }
 
 pub struct IconPickerStory {
-    icon: Entity<IconPickerState<PickableIcon>>,
-    selected_icon: Option<PickableIcon>,
+    picker_state_normal: Entity<IconPickerState<PickableIcon>>,
+    picker_state_colored: Entity<IconPickerState<PickableIcon>>,
+    selected_icon_normal: Option<PickableIcon>,
+    selected_icon_colored: Option<PickableIcon>,
     _subscriptions: Vec<Subscription>,
 }
 
@@ -45,19 +47,31 @@ impl IconPickerStory {
     }
 
     fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        let icon = cx.new(|cx| {
-            IconPickerState::new(window, cx).default_value(PickableIcon::GalleryVerticalEnd)
+        let picker_state_normal = cx.new(|cx| {
+            IconPickerState::new(window, cx).default_value(PickableIcon::ArrowUp)
+        });
+        let picker_state_colored = cx.new(|cx| {
+            IconPickerState::new(window, cx).default_value(PickableIcon::ArrowDown)
         });
 
-        let _subscriptions = vec![cx.subscribe(&icon, |this, _, ev, _| match ev {
-            IconPickerEvent::Change(icon) => {
-                this.selected_icon = *icon;
-            }
-        })];
+        let _subscriptions = vec![
+            cx.subscribe(&picker_state_normal, |this, _, ev, _| match ev {
+                IconPickerEvent::Change(icon) => {
+                    this.selected_icon_normal = *icon;
+                }
+            }),
+            cx.subscribe(&picker_state_colored, |this, _, ev, _| match ev {
+                IconPickerEvent::Change(icon) => {
+                    this.selected_icon_colored = *icon;
+                }
+            }),
+        ];
 
         Self {
-            icon,
-            selected_icon: Some(PickableIcon::GalleryVerticalEnd),
+            picker_state_normal,
+            picker_state_colored,
+            selected_icon_normal: Some(PickableIcon::ArrowUp),
+            selected_icon_colored: Some(PickableIcon::ArrowDown),
             _subscriptions,
         }
     }
@@ -65,19 +79,29 @@ impl IconPickerStory {
 
 impl Focusable for IconPickerStory {
     fn focus_handle(&self, cx: &gpui::App) -> gpui::FocusHandle {
-        self.icon.read(cx).focus_handle(cx)
+        self.picker_state_normal.read(cx).focus_handle(cx)
     }
 }
 
 impl Render for IconPickerStory {
     fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        v_flex().gap_3().child(
-            section("Normal")
-                .max_w_md()
-                .child(IconPicker::new(&self.icon))
-                .when_some(self.selected_icon, |this, icon| {
-                    this.child(div().w_48().child(icon.name()))
-                }),
-        )
+        v_flex()
+            .gap_3()
+            .child(
+                section("Normal")
+                    .max_w_md()
+                    .child(IconPicker::new(&self.picker_state_normal))
+                    .when_some(self.selected_icon_normal, |this, icon| {
+                        this.child(div().w_48().child(icon.name()))
+                    }),
+            )
+            .child(
+                section("Colored icons")
+                    .max_w_md()
+                    .child(IconPicker::new(&self.picker_state_colored).icons_color(Hsla::green()))
+                    .when_some(self.selected_icon_colored, |this, icon| {
+                        this.child(div().w_48().child(icon.name()))
+                    }),
+            )
     }
 }
